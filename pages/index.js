e71317a0914eade17852f15ee6f5e036f37e0150
elprofa <76491497+elprofa@users.noreply.github.com>
2021-05-28 22:38:34 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,42 @@
+import Head from 'next/head'
+// import BestDishes from '../components/molecule/BestDishes'
+// import PopularDishes from '../components/molecule/PopularDishes/'
+// import Service from '../components/molecule/Services';
+// import SliderAccueil from '../components/molecule/SliderAccueil';
+// import NewsLatter from '../components/newslatter';
+// import CardNewlatest from '../components/newlatestNews';
+// import Diapo from '../components/diapo';
+// import Footer from '../components/footer';
+// import MenuPizza from '../components/menuPizza';
+// import SectionPizza from '../components/SectionPizza';
+// import SectionPasta from '../components/SectionPasta';
+// import SectionBurgers from '../components/SectionBurgers';
+// import BookTable from '../components/SectionBookTable';
+// import BannerMenu from '../components/BannerMenu';
+
+export default function Home() {
+  return (
+    <div className="App">
+      <Head>
+        <title>CrocPiz</title>
+        <link rel="icon" href="/img/favicon.ico" />
+      </Head>
+
+      <main className="main">
+
+        wedo
+        {/* <SliderAccueil />
+        <BestDishes />
+        <div className="container">
+          <PopularDishes />
+          <Service />
+          <MenuPizza/>
+        </div>
+        <Diapo/>
+        <CardNewlatest/> */}
+      </main>
+      {/* <Footer/> */}
+
+    </div>
+  )
+}
